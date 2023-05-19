e121b9f831946206f97ffe29d0611ea6cc6df9e2
aguiarHugo <aguiarhugofx@gmail.com>
2023-05-18 23:27:10 -0300

    bg dark-300

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen bg-dark-400">
+    <main className="flex min-h-screen bg-dark-300">
       <h1>TEstando</h1>
     </main>
   )
