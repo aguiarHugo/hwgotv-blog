9a452bc98a2b2afba2ba4d3478a6ceac8b377b5b
aguiarHugo <aguiarhugofx@gmail.com>
2023-05-31 09:59:13 -0300

    deleted repeated content

--- src/app/pages/patchnotes/patchContent.ts
+++ src/app/pages/patchnotes/patchContent.ts
@@ -5,16 +5,4 @@ export const patchContent = [
     date: '14 de abril de 2023',
     snippet: 'Retrospectiva do Beta Aberto de Diablo IV: transformando feedback em mudanças'
   },
-  {
-    title: 'Beta Patch',
-    url: 'https://news.blizzard.com/pt-br/diablo4/23938289/retrospectiva-do-beta-aberto-de-diablo-iv-transformando-feedback-em-mudancas',
-    date: '14 de abril de 2023',
-    snippet: 'Retrospectiva do Beta Aberto de Diablo IV: transformando feedback em mudanças'
-  },
-  {
-    title: 'Beta Patch',
-    url: 'https://news.blizzard.com/pt-br/diablo4/23938289/retrospectiva-do-beta-aberto-de-diablo-iv-transformando-feedback-em-mudancas',
-    date: '14 de abril de 2023',
-    snippet: 'Retrospectiva do Beta Aberto de Diablo IV: transformando feedback em mudanças'
-  },
 ]
